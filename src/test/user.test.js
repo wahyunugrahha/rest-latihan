e2674920174fceabde4a2fc3e84669955db86cac
wahyunugrahha <wahyunih297@gmail.com>
2025-06-25 20:49:44 +0700

    add unit test

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -12,13 +12,23 @@ describe("POST /api/users", function () {
   it("should can register user", async () => {
     const result = await supertest(web).post("/api/users").send({
       username: "test",
-      passowrd: "test",
+      password: "test",
       name: "test user",
     });
-    console.log(result.body);
     expect(result.status).toBe(200);
     expect(result.body.data.username).toBe("test");
     expect(result.body.data.passowrd).toBeUndefined();
     expect(result.body.data.name).toBe("test user");
+  });
+
+  it("should reject if inpuut is invalid", async () => {
+    const result = await supertest(web).post("/api/users").send({
+      username: "",
+      password: "",
+      name: "",
+    });
+    console.log(result.body);
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
   });
 });
