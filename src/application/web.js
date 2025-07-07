22eb4d0792c1981739586c7615804d9375d12b0d
wahyunugrahha <wahyunih297@gmail.com>
2025-07-07 17:29:05 +0700

    add swagger

--- src/application/web.js
+++ src/application/web.js
@@ -2,9 +2,16 @@ import express from "express";
 import { publicRouter } from "../routes/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { userRouter } from "../routes/api.js";
-
+import swaggerUi from 'swagger-ui-express'; 
+import YAML from 'yamljs'; 
+import path from 'path'; 
 const web = express();
 web.use(express.json());
+
+// Swagger
+const swaggerDocument = YAML.load(path.resolve("docs", "swagger.yaml")); 
+web.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 web.use(publicRouter);
 web.use(userRouter);
 web.use(errorMiddleware);
